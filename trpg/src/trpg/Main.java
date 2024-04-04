ded3cc9336e12a0658c4d3cf701ef24c5fc8eecb
k0gang <k0gang@naver.com>
2024-04-04 10:41:16 +0900

    Add : Main

--- trpg/src/trpg/Main.java
+++ trpg/src/trpg/Main.java
@@ -2,6 +2,13 @@ package trpg;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		boolean run = true;
+		GameManager.instance.init();
+		while(true) {
+			run = GameManager.instance.stageChange();
+			if(run == false)
+				break;
+		}
+		System.out.println("게임종료");
 	}
 }
